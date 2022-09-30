12eaba1fa667132b9e2336d2ccc44b2955dcc316
Brian Nkhata <bnkhata@mra.mw>
2022-09-30 16:49:42 +0200

    WIP

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiselaWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Vat()
+        {
+            return View();
+        }
+    }
+}
