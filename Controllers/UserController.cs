3245f0397d734ff6eeb17f23f669b6cdee2cad96
briannkhata <briannkhata@gmail.com>
2022-10-03 20:23:23 +0200

    WIP

---
+++ Controllers/UserController.cs
@@ -0,0 +1,126 @@
+﻿using BiselaWeb.Data;
+using BiselaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kweb.Controllers
+{
+    public class UserController : Controller
+    {
+        BEntities db;
+        User user;
+        UserModel userModel;
+        // GET:User
+        public ActionResult Index()
+        {
+            List<vwUser> users = new List<vwUser>();
+            using (var db = new BEntities())
+            {
+                users = db.vwUsers.ToList();
+            }
+            return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //var gender = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            using (var db = new BEntities())
+            {
+                userModel = new UserModel()
+                {
+                    //roles = db.Roles.Where(x => x.Deleted == 0).ToList(),
+                    //shops = db.Shops.Where(x => x.Deleted == 0).ToList(),
+                    //userTypes = db.UserTypes.Where(x => x.Deleted == 0).ToList(),
+                    //genders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList(),
+                    //UserTypeId = 0,
+                    //RoleId = 0,
+                    //ShopId = 0,
+                };
+            }
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            int UserId = (int)Session["UserId"];
+            try
+            {
+                using (db = new BEntities())
+                {
+                    user = new User()
+                    {
+                        UserId = short.Parse(Request.Form["id"]),
+                        Name = Request.Form["Name"],
+                        UserName = Request.Form["UserName"],
+                        Email = Request.Form["Email"],
+                        Phone = Request.Form["Phone"],
+                        Gender = Request.Form["Gender"],
+                        PassWord = Request.Form["Phone"],
+                        ShopId = short.Parse(Request.Form["ShopId"]),
+                    };
+
+                    if (user.UserId > 0)
+                    {
+                        db.Entry(user).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Users.Add(user);
+                    }
+                    db.SaveChanges();
+                }
+                TempData["successMsg"] = "User Successfully Saved";
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id, UserModel userDetails)
+        {
+            using (var db = new BEntities())
+            {
+                var userSingle = db.Users.Where(x => x.UserId == id).SingleOrDefault();
+                userDetails = new UserModel()
+                {
+                    UserName = userSingle.UserName,
+                    UserId = userSingle.UserId,
+                    Name = userSingle.Name,
+                    Email = userSingle.Email,
+                    Phone = userSingle.Phone,
+                    Gender = userSingle.Gender,
+                };
+            }
+            return View("Create", userDetails);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new BEntities())
+                {
+                    var user = db.Users.Where(x => x.UserId == id).SingleOrDefault();
+                    user.Deleted = 1;
+                    db.Entry(user).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                TempData["successMsg"] = "User Successfully Deleted";
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiselaWeb.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Deleted { get; set; }
+    }
+   }
